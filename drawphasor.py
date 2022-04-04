9ba7a8104cec5df78081de578e92ec1ce31d1b8b
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-04-04 12:45:33 +0300

    Create drawphasor.py

---
+++ drawphasor.py
@@ -0,0 +1,22 @@
+import math
+import turtle
+import time
+
+class Drawer(object):
+    def __init__(self):
+        self.__phasors=[]
+    def add(self, phasor):
+        self.__phasors.append(phasor)
+        
+    def addM(self, phasors):
+        self.__phasors+=phasors
+    def graph(self,scalar, view_time, save=False):
+        t=turtle.Turtle()
+        t.speed(scalar*len(self.__phasors))
+        for phasor in self.__phasors:
+            t.left(phasor[1]/math.pi*180)
+            
+            t.forward(scalar*phasor[0])
+        time.sleep(view_time)
+        turtle.bye()
+        
